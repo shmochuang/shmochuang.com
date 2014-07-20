6223403ab585dcb5bbee21d14648fe6d30fc2f87
Shmo Chuang <elisajchuang@gmail.com>
2014-07-19 22:41:06 -0400

    switched to bootstrap

--- index.php
+++ index.php
@@ -5,22 +5,24 @@
     <title>Elisa "Shmo" Chuang</title>
     
     <link rel= "icon" type = "image/ico" href="images/favicon.ico"> <!-- favicon for site -->
-    <link rel= "stylesheet" type = "text/css" href="css/blueprint/screen.css" media="screen, projection"> <!-- link to blueprint/screen.css -->
-    <link rel= "stylesheet" type = "text/css" href="css/blueprint/print.css" media="print"> <!-- link to blueprint/print.css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- link to bootstrap css -->
     <link rel= "stylesheet" type="text/css" href="css/style.css"> <!-- link to custom stylesheet -->
     <link href='http://fonts.googleapis.com/css?family=Raleway|Lobster' rel='stylesheet' type='text/css'> <!-- link to fonts -->
 </head>
 <body>
-    <div class="span-24">
+    <div class="row"> 
       <h1><img src="images/snowing7.png" alt=="snowflake"> Shmo <img src="images/snowing7.png" alt=="snowflake"></h1>
-    </div>
-    <div class="span-12 imgdiv">.
-      <img class="span-12 pic" src="images/me.jpg" alt="Me at Disney World.">
-    </div>
-    <div class="span-12 last">
-        <h2>Projects</h2>
-        <a href="http://p2.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 2: Password Generator</a><br>
-        <a href="http://p3.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 3: Developer's Best Friend</a>
-    </div>
+    </div> <!-- end header row -->
+    
+    <div class="row">
+	    <div class="col-xs-12 col-md-6">
+	      <img class = "pic" src="images/me.jpg" alt="Me at Disney World.">
+	    </div>
+	    <div class="col-xs-12 col-md-6">
+	        <h2>Projects</h2>
+	        <a href="http://p2.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 2: Password Generator</a><br>
+	        <a href="http://p3.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 3: Developer's Best Friend</a>
+	    </div>
+    </div> <!-- end content row -->
 </body>
 </html>
