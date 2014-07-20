4f6ed98f933c235da8df2ff5416c896dbb38b350
Shmo Chuang <elisajchuang@gmail.com>
2014-07-20 13:17:28 -0400

    added about me hover

--- index.php
+++ index.php
@@ -8,17 +8,25 @@
     <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- link to bootstrap css -->
     <link rel= "stylesheet" type="text/css" href="css/style.css"> <!-- link to custom stylesheet -->
     <link href='http://fonts.googleapis.com/css?family=Raleway|Lobster' rel='stylesheet' type='text/css'> <!-- link to fonts -->
+
 </head>
 <body>
     <div class="row"> 
-      <h1><img src="images/snowing7.png" alt=="snowflake"> Shmo <img src="images/snowing7.png" alt=="snowflake"></h1>
+      <h1>Shmo</h1>
     </div> <!-- end header row -->
     
     <div class="row">
-	    <div class="col-xs-12 col-md-6">
-	      <img class = "pic" src="images/me.jpg" alt="Me at Disney World.">
+	    <div class="col-xs-12 col-md-6" id="pic-div">
+	      <a href="/" id="pic-a">
+		      <img id="pic" src="images/me.jpg" alt="Me at Disney World.">
+		      <span class="about-me-txt">
+		      	<span>
+		      		My name is Elisa Chuang, but everybody calls me "Shmo". I'm currently a live-in nanny while also pursuing my ALM in Digital Media and Instructional Design from Harvard. I hope to become a web developer in the future. Hopefully I'll be able to do that soon. This site is my portfolio. Thanks for taking a look.
+		      	</span>
+		      </span> 
+	      </a>
 	    </div>
-	    <div class="col-xs-12 col-md-6">
+	    <div class="col-xs-12 col-md-6" id="projects">
 	        <h2>Projects</h2>
 	        <a href="http://p2.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 2: Password Generator</a><br>
 	        <a href="http://p3.shmochuang.com" alt="dwa15 p2 project">DWA15 - Project 3: Developer's Best Friend</a>
